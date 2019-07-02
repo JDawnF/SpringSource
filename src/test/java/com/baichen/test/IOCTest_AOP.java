4f4792ed99469d6a309313d3c2ec040100ca08f9
JDawnF <335825732@qq.com>
2019-07-02 23:52:06 +0800

    AOP配置类和测试类

---
+++ src/test/java/com/baichen/test/IOCTest_AOP.java
@@ -0,0 +1,27 @@
+package com.baichen.test;
+
+import com.baichen.aop.MathCalculator;
+import com.baichen.config.MainConfigOfAOP;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @Program: IOCTest_AOP
+ * @Author: baichen
+ * @Description: AOP测试类
+ */
+public class IOCTest_AOP {
+    @Test
+    public void test01(){
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfAOP.class);
+
+        //1、不要字节创建对象，要从容器中获取
+//		MathCalculator mathCalculator = new MathCalculator();
+//		mathCalculator.div(1, 1);
+        MathCalculator mathCalculator = applicationContext.getBean(MathCalculator.class);
+
+        mathCalculator.div(1, 1);
+
+        applicationContext.close();
+    }
+}
